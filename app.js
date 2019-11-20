1476f6df29709b4d1d1cb5f8d41866a65cb95ace
Rayan Mahanna <rayan.mahanna@devinci.fr>
2019-11-20 15:18:35 +0100

    added objects file

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const app = express();
 
-app.use((req, res, next) => {
-  res.status(200).json({
-    message: "Working"
-  });
-});
+const objectRoutes = require("./api/routes/objects");
+
+app.use("./objects", objectRoutes);
 
 module.exports = app;
